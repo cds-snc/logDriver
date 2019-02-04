76a14d8e581b2b7dacb63a5f605ddccf8615941c
Tim Arney <timarney@users.noreply.github.com>
2019-02-04 14:47:09 -0500

    Update tests (#8)

--- src/lib/adapters/StackDriver/StackDriverClient.test.js
+++ src/lib/adapters/StackDriver/StackDriverClient.test.js
@@ -1,14 +1,8 @@
-import Logger from "../../logger";
-import { StackDriverClient } from "./StackDriverClient";
+import { StackDriverClient as Driver } from "./StackDriverClient";
+import { checkLogger } from "../util/checkLogger";
 
 describe("StackDriverClient", () => {
-  it("Subscribes to logger and receives message", () => {
-    StackDriverClient.log = jest.fn();
-    const callee = StackDriverClient.log;
-    Logger.subscribe("error", callee);
-    Logger.debug("The client message");
-    expect(callee).toBeCalled();
-    expect(callee.mock.calls[0][0]).toBe("debug");
-    expect(callee.mock.calls[0][1].data).toBe("The client message");
+  it("Subscribes to StackDriverClient + receives message", () => {
+    checkLogger(Driver);
   });
 });
